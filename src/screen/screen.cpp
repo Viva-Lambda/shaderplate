6d1879d3dd6dd94b87cb3b1ccce302f4f1f70fd2
qm.auber <qm.auber@tutanota.com>
2020-07-02 08:58:39 +0200

    screen space ray tracing started but will do screen space ambient occlusion first

---
+++ src/screen/screen.cpp
@@ -0,0 +1,34 @@
+// screen space ray tracing implementation effort
+#include <custom/utils.hpp>
+
+int main() {
+  initializeGLFWMajorMinor(4, 3);
+  GLFWwindow *window = glfwCreateWindow(
+      WINWIDTH, WINHEIGHT, "Lightprobs Shading Example", NULL, NULL);
+
+  if (window == NULL) {
+    std::cout << "Loading GLFW window had failed" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  //
+  // dealing with mouse actions
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, mouse_scroll_callback);
+
+  // deal with input method
+  // glfw should capture cursor movement as well
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  // load opengl function
+  if (gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)) == 0) {
+    std::cout << "Failed to start glad" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // set default view port
+  glViewport(0, 0, WINWIDTH, WINHEIGHT);
+}
